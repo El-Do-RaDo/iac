510d21e672637436aab909b1f426e04c540ceee1
Aniket Sangohi <as@kali-linux.ccobravo.net>
2025-04-03 15:46:36 +0530

    Updated the resources for Azure vnets...

---
+++ azure/modules/vnets/main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  dns_servers         = var.dns_servers
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  for_each             = { for s in var.subnets : s.name => s }
+  name                 = each.value.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = each.value.address_prefixes
+
+  delegation {
+    name = each.value.delegation_name
+    service_delegation {
+      name = each.value.service_delegation_name
+      actions = each.value.actions
+    }
+  }
+
+  depends_on = [azurerm_virtual_network.this]
+}
